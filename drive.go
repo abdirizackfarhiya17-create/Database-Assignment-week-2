5b091c37137648c52657b56aca9d7883a5f5e1ae
abdirizackfarhiya17-create <abdirizackfarhiya17@gmail.com>
2026-09-09 21:55:40 +0300

    Update drive.go

--- drive.go
+++ drive.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
-    // Left blank for SQL autograder verification
+	// Printing queries directly for the automated test runner
+	fmt.Println(query1)
+	fmt.Println(query2)
+	fmt.Println(query3)
+	fmt.Println(query4)
+	fmt.Println(query5)
 }
+
+// Question 1: Retrieve Payment Information
+var query1 = `SELECT checkNumber, paymentDate, amount FROM payments;`
+
+// Question 2: Find Orders in Process
+var query2 = `SELECT orderDate, requiredDate, status FROM orders WHERE status = 'In Process' ORDER BY orderDate DESC;`
+
+// Question 3: Find Sales Representatives
+var query3 = `SELECT firstName, lastName, email FROM employees WHERE jobTitle = 'Sales Rep' ORDER BY lastName;`
+
+// Question 4: Retrieve All Columns from the Office Table
+var query4 = `SELECT * FROM offices;`
+
+// Question 5: Retrieve the Top Discounted Products
+var query5 = `SELECT productName, productLine FROM products ORDER BY buyPrice DESC LIMIT 5;`
